2fceeb5af5a2b0fe5779a6c571111a61a4ba3ded
Kamariw95 <kamarimw@gmail.com>
2020-05-19 09:45:46 -0400

    Changes index from function component to class component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -9,40 +9,44 @@ import SEO from "../components/seo"
 
 import '../styles/index.css'
 
-const IndexPage = (props: PageProps) => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="main-container">
-      <div className="container left-container">
-        <div className="gatsby-img">
-          <Image />
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <div className="main-container">
+          <div className="container left-container">
+            <div className="gatsby-img">
+              <Image />
+            </div>
+          </div>
+          <div className="container right-container">
+            <div className="sign-up-form">
+              <div className="sign-up-row">
+                <label className="container-label">Username</label>
+                <input className="sign-up-input" type="text"/>
+              </div>
+              <div className="sign-up-row">
+                <label className="container-label">Email</label>
+                <input className="sign-up-input" type="text"/>
+              </div>
+              <div className="sign-up-row">
+                <label className="container-label">Password</label>
+                <input className="sign-up-input" type="password"/>
+              </div>
+              <div className="sign-up-row">
+                <label className="container-label">Confirm Password</label>
+                <input className="sign-up-input" type="password"/>
+              </div>
+              <div className="button-row">
+                <button className="submit-button" disabled={false}>Submit</button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="container right-container">
-        <div className="sign-up-form">
-          <div className="sign-up-row">
-            <label className="container-label">Username</label>
-            <input className="sign-up-input" type="text"/>
-          </div>
-          <div className="sign-up-row">
-            <label className="container-label">Email</label>
-            <input className="sign-up-input" type="text"/>
-          </div>
-          <div className="sign-up-row">
-            <label className="container-label">Password</label>
-            <input className="sign-up-input" type="password"/>
-          </div>
-          <div className="sign-up-row">
-            <label className="container-label">Confirm Password</label>
-            <input className="sign-up-input" type="password"/>
-          </div>
-          <div className="button-row">
-            <button className="submit-button" disabled={true}>Submit</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </Layout>
-)
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
