49ce6f5647a0793c5e3be20ae8769c6868ed026e
Kamariw95 <kamarimw@gmail.com>
2020-05-20 10:43:46 -0400

    sets up input fields

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -44,19 +44,19 @@ class IndexPage extends React.Component<IState> {
             <div className="sign-up-form">
               <div className="sign-up-row">
                 <label className="container-label">Username</label>
-                <input className="sign-up-input" type="text"/>
+                <input className="sign-up-input" name="username" maxLength={15} type="text" onChange={this.handleChange}/>
               </div>
               <div className="sign-up-row">
                 <label className="container-label">Email</label>
-                <input className="sign-up-input" type="text"/>
+                <input className="sign-up-input" name="email" type="email" onChange={this.handleChange}/>
               </div>
               <div className="sign-up-row">
                 <label className="container-label">Password</label>
-                <input className="sign-up-input" type="password"/>
+                <input className="sign-up-input" name="password" type="password" onChange={this.handleChange}/>
               </div>
               <div className="sign-up-row">
                 <label className="container-label">Confirm Password</label>
-                <input className="sign-up-input" type="password"/>
+                <input className="sign-up-input" name="confirm-password" type="password" onChange={this.handleChange}/>
               </div>
               <div className="button-row">
                 <button className="submit-button" disabled={false}>Submit</button>
